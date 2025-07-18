0a4b2358fdbc0b8a4f267634ef25134ac633037e
Nidhin Pushpakaran <nidhinp.412@gmail.com>
2025-07-18 12:36:54 +1200

    Just the demo files update

--- hello-world.py
+++ hello-world.py
@@ -3,8 +3,6 @@ import os
 
 def hello_world():
     print('hello world')
-    print('hello world again')
-    print('hello world one more time')
     print(os.getcwd())
 
 
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 function hello_world() {
   console.log("hello world!");
+  console.error("hello world error!");
 
   var hello;
 }
-
 
