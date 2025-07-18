fa6ff0cd8e2b8f8acbb51f86e617f76c982f152d
Nidhin Pushpakaran <nidhinp.412@gmail.com>
2025-07-18 12:29:16 +1200

    Remapped copilot suggestion acception key

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,11 @@
+import os
+
+
 def hello_world():
     print('hello world')
+    print('hello world again')
+    print('hello world one more time')
+    print(os.getcwd())
 
 
 if __name__ == '__main__':
